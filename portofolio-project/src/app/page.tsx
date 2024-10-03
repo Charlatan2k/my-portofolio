954e21860dd6856f4253a83a862ea62a709e0fef
Charlatan2k <octavianmihai.calin@gmail.com>
2024-10-04 00:59:07 +0200

    refactor: Finished navbar/header with button

--- portofolio-project/src/app/page.tsx
+++ portofolio-project/src/app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
             width={25}
             height={25}
             alt='Spanish Flag Icon'
-            className='ml-[0.75rem] mt-[0.9375rem]'
+            className='ml-[0.75rem] mt-[0.9375rem] cursor-pointer'
           />
           <Image
-            className='ml-[2.3125rem]'
+            className='ml-[2.3125rem] cursor-pointer'
             src={'/octavian-logo.png'}
             alt='Logo'
             width={98}
@@ -22,10 +22,12 @@ export default function Home() {
           />
         </div>
         <div className='flex items-center gap-16 text-2xl font-light mr-11'>
-          <h4 className='navbar-text'>About</h4>
-          <h4 className='navbar-text'>Skills</h4>
-          <h4 className='navbar-text'>Projects</h4>
-          <h4 className='navbar-text'>Contact</h4>
+          <a href='/' className='navbar-text cursor-pointer'>
+            About
+          </a>
+          <a className='navbar-text cursor-pointer'>Skills</a>
+          <a className='navbar-text cursor-pointer'>Projects</a>
+          <a className='navbar-text cursor-pointer'>Contact</a>
           <Button children='Resume' />
         </div>
       </header>
