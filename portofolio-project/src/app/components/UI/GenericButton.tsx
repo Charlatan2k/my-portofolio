b558ae9f6c25c015165c124b3659a02022796e74
Charlatan2k <octavianmihai.calin@gmail.com>
2024-10-05 16:52:17 +0200

    feat: Add size prop for button

--- portofolio-project/src/app/components/UI/GenericButton.tsx
+++ portofolio-project/src/app/components/UI/GenericButton.tsx
@@ -4,15 +4,39 @@ import styled from 'styled-components';
 
 interface ButtonProps {
   children: string;
-  className: string;
+  className?: string;
+  size?: 'big';
 }
 
-const GenericButton = styled.button`
-  width: 146px;
-  height: 50px;
+const GenericButton = styled.button.attrs((props) => ({
+  className: props.className,
+}))<ButtonProps>`
+  width: ${(props) => {
+    switch (props.size) {
+      case 'big':
+        return '210px';
+      default:
+        return '146px';
+    }
+  }};
+  height: ${(props) => {
+    switch (props.size) {
+      case 'big':
+        return '60px';
+      default:
+        return '50px';
+    }
+  }};
   border: 2px solid #5dd2c0;
   border-radius: 5px;
-  font-size: 20px;
+  font-size: ${(props) => {
+    switch (props.size) {
+      case 'big':
+        return '25px';
+      default:
+        return '20px';
+    }
+  }};
   font-family: 'SF Mono', 'Courier New', 'Consolas', 'Lucida Console', monospace;
   text-align: center;
   transition: all 0.12s ease-in-out;
@@ -24,8 +48,12 @@ const GenericButton = styled.button`
   }
 `;
 
-const Button = ({ children, className }: ButtonProps) => {
-  return <GenericButton className={className}>{children}</GenericButton>;
+const Button = ({ children, className, size }: ButtonProps) => {
+  return (
+    <GenericButton className={className} size={size}>
+      {children}
+    </GenericButton>
+  );
 };
 
 export default Button;
