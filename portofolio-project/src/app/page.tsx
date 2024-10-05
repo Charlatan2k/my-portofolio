edede43813a43e104a73147160bb5182bb16a052
Charlatan2k <octavianmihai.calin@gmail.com>
2024-10-05 16:29:04 +0200

    feat: Add main text to home section

--- portofolio-project/src/app/page.tsx
+++ portofolio-project/src/app/page.tsx
@@ -3,7 +3,7 @@ import Button from './components/UI/GenericButton';
 
 export default function Home() {
   return (
-    <div className='h-screen'>
+    <div className='h-screen flex flex-col'>
       <header className='border-b-[3px] justify-between flex border-b-[#2A2A2A] h-36 shadow-[0_4px_10px_rgba(0,0,0,0.25)]'>
         <div className='logo-cont'>
           <Image
@@ -28,9 +28,32 @@ export default function Home() {
           <a className='navbar-text cursor-pointer'>Skills</a>
           <a className='navbar-text cursor-pointer'>Projects</a>
           <a className='navbar-text cursor-pointer'>Contact</a>
-          <Button children='Resume' />
+          <Button>Resume</Button>
         </div>
       </header>
+
+      <main className='flex flex-grow flex-col items-center'>
+        <section className='home-section items-center h-full w-full flex-col flex'>
+          <div className='home-text mt-40 border-2 border-blue-500 flex flex-col items-center'>
+            <h6 className='font-sf-mono font-medium mb-12 text-3xl text-[var(--primary-blue)]'>
+              Welcome, my name is
+            </h6>
+            <div className='flex flex-col items-center'>
+              <h1 className='text-5xl font-bold'>Octavian Mihai Calin</h1>
+              <h2 className='text-4xl font-bold opacity-75'>
+                Crafting stories through code
+              </h2>
+            </div>
+            <p className='text-xl font-bold opacity-75 w-[863px] text-center mt-7'>
+              I’m a Full Stack web developer passionate about crafting engaging
+              digital experiences, blending front-end, back-end development, and
+              design. Currently, I’m focused on honing my skills and advancing
+              my craft.
+            </p>
+            <Button>Explore</Button>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
