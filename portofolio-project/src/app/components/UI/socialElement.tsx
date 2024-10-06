df3ecfde103d56ce4707268dba87592df567f001
Charlatan2k <octavianmihai.calin@gmail.com>
2024-10-06 12:15:27 +0200

    refactor: unfinished socialElement (pending external icon)

--- portofolio-project/src/app/components/UI/socialElement.tsx
+++ portofolio-project/src/app/components/UI/socialElement.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import SideElement from './SideElementLine';
 import { socialMedia } from '@/config';
+import Icon from './IconComponent';
 
 const StyledSocialList = styled.ul`
   display: flex;
@@ -46,12 +47,9 @@ const Social = () => (
       {socialMedia &&
         socialMedia.map(({ url, name }, i) => (
           <li key={i}>
-            <a
-              href={url}
-              aria-label={name}
-              target='_blank'
-              rel='noreferrer'
-            ></a>
+            <a href={url} aria-label={name} target='_blank' rel='noreferrer'>
+              <Icon name={name} />
+            </a>
           </li>
         ))}
     </StyledSocialList>
