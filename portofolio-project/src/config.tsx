26c1d5cea8b7ce2e6ee12c14956bce204ca98d5f
Charlatan2k <octavianmihai.calin@gmail.com>
2024-10-06 11:44:01 +0200

    feat: Add config file with email and social links objects

---
+++ portofolio-project/src/config.tsx
@@ -0,0 +1,18 @@
+module.exports = {
+  email: 'octavianmihai.calin@gmail.com',
+
+  socialMedia: [
+    {
+      name: 'Github',
+      url: 'https://github.com/Charlatan2k',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/octavio_calin/',
+    },
+    {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/octavian-calin/',
+    },
+  ],
+};
