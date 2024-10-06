4a8b97a797b6b042103e0df24b014eca1e023a67
Charlatan2k <octavianmihai.calin@gmail.com>
2024-10-06 11:49:56 +0200

    refactor: Change config.tsx export to tsx standard

--- portofolio-project/src/config.tsx
+++ portofolio-project/src/config.tsx
@@ -1,18 +1,16 @@
-module.exports = {
-  email: 'octavianmihai.calin@gmail.com',
+export const email = 'octavianmihai.calin@gmail.com';
 
-  socialMedia: [
-    {
-      name: 'Github',
-      url: 'https://github.com/Charlatan2k',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/octavio_calin/',
-    },
-    {
-      name: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/octavian-calin/',
-    },
-  ],
-};
+export const socialMedia = [
+  {
+    name: 'Github',
+    url: 'https://github.com/Charlatan2k',
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/octavio_calin/',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/octavian-calin/',
+  },
+];
