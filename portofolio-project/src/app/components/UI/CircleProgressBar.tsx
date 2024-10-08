88d1bfa720ef43af2ae5f453de307600136aab44
Charlatan2k <octavianmihai.calin@gmail.com>
2024-10-08 19:55:36 +0200

    feat: add text by prop to CircularProgressBar component

--- portofolio-project/src/app/components/UI/CircleProgressBar.tsx
+++ portofolio-project/src/app/components/UI/CircleProgressBar.tsx
@@ -1,37 +1,41 @@
 interface ProgressBarProps {
   percentage: number;
+  text: string;
 }
 
-const CircularProgressBar = ({ percentage }: ProgressBarProps) => {
+const CircularProgressBar = ({ percentage, text }: ProgressBarProps) => {
   const strokeDasharray = `${percentage * 2.8} ${280 - percentage * 2.8}`;
 
   return (
-    <div className='flex items-center justify-center'>
-      {/* Outer Circle */}
-      <svg className='w-28 h-28' style={{ transform: 'rotate(-50deg)' }}>
-        <circle
-          cx='50%'
-          cy='50%'
-          r='45%'
-          stroke='var(--primary-gray)'
-          strokeWidth='10'
-          fill='transparent'
-        />
-        <circle
-          cx='50%'
-          cy='50%'
-          r='45%'
-          stroke='var(--primary-blue)'
-          strokeWidth='10'
-          fill='transparent'
-          strokeDasharray={strokeDasharray}
-          strokeDashoffset='0'
-          strokeLinecap='round'
-          style={{ transition: 'stroke-dasharray 0.5s ease-in-out' }}
-        />
-      </svg>
-      {/* Percentage Text */}
-      <span className='absolute text-xl font-bold'>{percentage}%</span>
+    <div className='flex flex-col items-center gap-4'>
+      <div className='flex items-center justify-center'>
+        {/* Outer Circle */}
+        <svg className='w-28 h-28' style={{ transform: 'rotate(-50deg)' }}>
+          <circle
+            cx='50%'
+            cy='50%'
+            r='45%'
+            stroke='var(--primary-gray)'
+            strokeWidth='10'
+            fill='transparent'
+          />
+          <circle
+            cx='50%'
+            cy='50%'
+            r='45%'
+            stroke='var(--primary-blue)'
+            strokeWidth='10'
+            fill='transparent'
+            strokeDasharray={strokeDasharray}
+            strokeDashoffset='0'
+            strokeLinecap='round'
+            style={{ transition: 'stroke-dasharray 0.5s ease-in-out' }}
+          />
+        </svg>
+        {/* Percentage Text */}
+        <span className='absolute text-xl font-bold'>{percentage}%</span>
+      </div>
+      <p className='font-bold text-xl'>{text}</p>
     </div>
   );
 };
