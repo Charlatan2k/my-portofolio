16569a031c0ff0d093b69cc3b42d76de36bd41e1
Charlatan2k <octavianmihai.calin@gmail.com>
2024-11-12 18:14:42 +0100

    feat: finished skills section

--- portofolio-project/src/app/components/Sections/Skills.tsx
+++ portofolio-project/src/app/components/Sections/Skills.tsx
@@ -1,4 +1,5 @@
 import Skill from '../UI/Skill';
+import { Icon } from '@iconify/react';
 
 export default function Skills() {
   return (
@@ -15,11 +16,86 @@ export default function Skills() {
       <div className='border-[3px] border-[var(--primary-gray)] h-24'></div>
       <div
         id='skills-content-box'
-        className='border-[5px] border-[var(--primary-gray)] rounded-xl h-[40%] w-[70%] flex justify-center items-center'
+        className='border-[5px] border-[var(--primary-gray)] rounded-xl h-[48%] w-[80%] flex justify-center items-center'
       >
-        <div className='flex flex-col border-r-[5px] border-[var(--primary-gray)] h-full justify-between w-full'></div>
-        <div className='flex flex-col border-r-[5px] border-[var(--primary-gray)] h-full justify-between w-full'></div>
-        <div className='flex flex-col h-full justify-between w-full'></div>
+        <section className='flex flex-col border-r-[5px] border-[var(--primary-gray)] h-full justify-between w-[90%]'>
+          <div className='flex flex-col items-center justify-center h-full'>
+            <div className='flex flex-col justify-center items-center'>
+              <Icon
+                className='border-2 border-[var(--primary-gray)] rounded-lg bg-[var(--primary-gray)] text-[var(--primary-blue)] p-2'
+                width={70}
+                height={70}
+                icon='material-symbols:database'
+              />
+              <h1 className='font-mono text-2xl tracking-[4px] text-center my-2 border-b-2 h-10 w-36 border-[var(--primary-blue)]'>
+                Back
+              </h1>
+            </div>
+            <div className=' w-[80%] h-auto flex gap-6 flex-wrap items-center justify-center mt-6'>
+              <Skill name='Node' />
+              <Skill name='Express' />
+              <Skill name='MySQL' />
+              <Skill name='MongoDB' />
+              <Skill name='Python' />
+              <Skill name='API REST' />
+              <Skill name='Amazon Web Service' />
+              <Skill name='Fiware' />
+            </div>
+          </div>
+        </section>
+        <div className='flex flex-col border-r-[5px] border-[var(--primary-gray)] h-full justify-between w-full'>
+          <div className='flex  flex-col items-center justify-center h-full'>
+            <div className='flex flex-col justify-center items-center'>
+              <Icon
+                className='border-2 border-[var(--primary-gray)] rounded-lg bg-[var(--primary-gray)] text-[var(--primary-blue)] p-3'
+                width={70}
+                height={70}
+                icon='bi:front'
+              />
+              <h1 className='font-mono text-2xl tracking-[4px] text-center my-2 border-b-2 h-10 w-36 border-[var(--primary-blue)]'>
+                Front
+              </h1>
+            </div>
+            <div className=' w-[94%] h-auto flex gap-6 flex-wrap items-center justify-center mt-6'>
+              <Skill name='HTML' />
+              <Skill name='CSS' />
+              <Skill name='JavaScript' />
+              <Skill name='JQuery' />
+              <Skill name='React' />
+              <Skill name='Angular' />
+              <Skill name='Next' />
+              <Skill name='Tailwind CSS' />
+              <Skill name='Typescript' />
+              <Skill name='Bootstrap' />
+            </div>
+          </div>
+        </div>
+        <div className='flex flex-col h-full justify-between w-[90%]'>
+          <div className='flex  flex-col items-center justify-center h-full'>
+            <div className='flex flex-col justify-center items-center'>
+              <Icon
+                className='border-2 border-[var(--primary-gray)] rounded-lg bg-[var(--primary-gray)] text-[var(--primary-blue)] p-2'
+                width={70}
+                height={70}
+                icon='clarity:tools-solid'
+              />
+              <h1 className='font-mono text-2xl tracking-[4px] text-center my-2 border-b-2 h-10 w-36 border-[var(--primary-blue)]'>
+                Tools
+              </h1>
+            </div>
+            <div className=' w-[100%] h-auto flex gap-6 flex-wrap items-center justify-center mt-6'>
+              <Skill name='GIT' />
+              <Skill name='Figma' />
+              <Skill name='Docker' />
+              <Skill name='Photoshop' />
+              <Skill name='Github' />
+              <Skill name='Sony Vegas' />
+              <Skill name='SCRUM' />
+              <Skill name='Premier Pro' />
+              <Skill name='DaVinci Resolve' />
+            </div>
+          </div>
+        </div>
       </div>
       <div className='border-[3px] border-[var(--primary-gray)] h-24'></div>
     </section>
